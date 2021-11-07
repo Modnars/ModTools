776265b67ee28345503c2a6dcaec8f4abf4cd126
ModnarShen <modnar@foxmail.com>
2021-11-07 21:37:57 +0800

    update: update file constructure

---
+++ XlsxParser/src/config.py
@@ -0,0 +1,11 @@
+'''
+version: 
+Author: modnarshen
+Date: 2021/06/30 19:47:51
+LastEditors: modnarshen
+LastEditTime: 2021/06/30 19:54:00
+Description: 
+'''
+
+PROJ_NAME = "XLSX_PARSER"
+DEBUG_MODE = True
--- XlsxParser/src/parse_xlsx.py
+++ XlsxParser/src/parse_xlsx.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
+
 '''
 Description: 导表工具
 version: 1.0.0
 Author: modnarshen
 Date: 2021/01/19 21:19:57
 LastEditors: modnarshen
-LastEditTime: 2021/04/09 10:00:08
+LastEditTime: 2021/06/30 19:53:16
 '''
+
 import sys
-import os
+
+import log
 
 # PYTHON_VERSION
 # OS_PLATFORM
 
-NUM_TYPES = set(('int32', 'uint32', 'int64', 'uint64', 'float'))
-BASIC_DATA_TYPES = set(('bool', 'int32', 'uint32', 'string'))
-EXTEND_DATA_TYPES = set(('struct'))
-
-class Field(object):
-    def __init__(self):
-        pass
-
-def read_data():
-    pass
+NUM_TYPES = set(("int32", "uint32", "int64", "uint64", "float"))
+BASIC_DATA_TYPES = set(("bool", "int32", "uint32", "string"))
+EXTEND_DATA_TYPES = set(("struct"))
 
 def main():
-    for item in BASIC_DATA_TYPES:
-        print(item)
     pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
